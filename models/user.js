a8649b6b6166239fa4a59fa265b720840842e6f0
Ajayi Oluwafemi Adeyinka <ajayiadeyinka6991@gmail.com>
2019-07-14 08:21:59 +0100

    added both signup endpoint and login endpoint

---
+++ models/user.js
@@ -0,0 +1,47 @@
+// Schema for User
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema
+var bcrypt=require('bcryptjs');
+const autoIncrement = require('mongoose-auto-increment');
+
+var UserSchema=mongoose.Schema({
+    _id:{
+        type:Number,
+        unique: true,
+    },
+    name:{
+    	type:String,
+        require:true,
+        trim: true, 
+        unique: true,
+    },
+    email:{
+       type:String,
+       require:true,
+       trim: true, 
+       unique: true,
+       match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    },
+    password:{
+        type:String,
+        require:true
+     },
+     createdAt:{
+        type:Date,
+    	require:true
+    },
+    updatedAt:{
+        type:Date,
+    	require:true
+    },
+});
+
+UserSchema.statics.hashPassword=function hashPassword(password){
+    return bcrypt.hashSync(password,10)
+}
+UserSchema.methods.isValid=function (hashedpassword) {
+    return bcrypt.compareSync(hashedpassword,this.password);
+}
+
+UserSchema.plugin(autoIncrement.plugin, 'User');
+module.exports=mongoose.model('User',UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,59 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const  jwt  =  require('jsonwebtoken');
+const User = require('../models/user');
 
-// /* GET authentication section of the API. */
+/* GET authentication section of the API. */
 router.get('/', function(req, res, next) {
   res.send('Api authentication section for user');
 });
+// router for user registration
+router.post('/signup', function (req,res) {
+  const user = new User({
+    name:req.body.name,
+    email:req.body.email,
+    password:User.hashPassword(req.body.password),
+    createdAt:Date.now()
+  });
+  user.save(function (err,result) {
+    if (err) { return res.status(501).json({message:'User cannot be created',})
+    }else{
+     User.findUserByEmail(email, (err, user)=>{
+        if (err) return  res.status(500).json({message:'Server error',}) 
+        if (user) {
+          return res.status(500).json({message:'User with this email already exist'})
+        } else {
+          const  expiresIn  =  24  *  60  *  60;
+          const  accessToken  =  jwt.sign({ id:  user.id }, process.env.SECRET_KEY, {
+              expiresIn:  expiresIn
+          });
+          return  res.status(200).send({
+             "user":  result,
+             "access_token":  accessToken,
+             "expires_in":  expiresIn ,  
+              message:'User created successfully',        
+          });
+        }
+  })  
+}
+})
+})
+// router for user login
+router.post('/login', function(req, res) {
+  const  email  =  req.body.email;
+  const  password  =  req.body.password;
+  User.findUserByEmail(email, (err, user)=>{
+        if (err) return  res.status(500).send('Server error!');
+        if (!user) return  res.status(404).send('User not found!');
+        const  result  =   User.isValid(password, user.password);
+        if(!result) return  res.status(401).send('Password not valid!');
 
+        const  expiresIn  =  24  *  60  *  60;
+        const  accessToken  =  jwt.sign({ id:  user.id }, process.env.SECRET_KEY, {
+            expiresIn:  expiresIn
+        });
+        res.status(200).send({ "user":  user, "access_token":  accessToken, "expires_in":  expiresIn});
+    });
+}); 
 module.exports = router;
  
