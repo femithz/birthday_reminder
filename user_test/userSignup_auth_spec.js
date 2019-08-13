0e3955b9d5702059a95444723ec3be57fe1cb16a
Ajayi Oluwafemi Adeyinka <ajayiadeyinka6991@gmail.com>
2019-08-13 03:19:50 +0100

    test file for signup function added

---
+++ user_test/userSignup_auth_spec.js
@@ -0,0 +1,43 @@
+/*jshint esversion: 6 */
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+const expect = require('chai').expect;
+const app = require('../app');
+
+
+// section to test for registering user
+describe('/POST user', () => {
+    it('it should not POST a user without  field', (done) => {
+        let user = {
+            name: "Ajayi Oluwafemi",
+            email: "test@gamil.com",
+            password: 2019,
+            createdAt:Date.now()
+        }
+      chai.request(app)
+          .post('/signup')
+          .send(user)
+          .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('errors');
+            done();
+          });
+    });
+    it('Fail, to register user', (done) => {
+        let user = {
+            name: "Ajayi Oluwafemi",
+            password: 2019,
+            createdAt:Date.now()
+        }
+      chai.request(app)
+          .post('/signup')
+          .send(user)
+          .end((err, res) => {
+                res.should.have.status(501);
+                const body = res.user;
+                expect(body.errors.text.name).to.equal('ValidatorError')
+            done();
+          });
+    });
+});
