49a98839d8ddb111a19854f7b4cad621f1fdac98
Ajayi Oluwafemi Adeyinka <ajayiadeyinka6991@gmail.com>
2019-08-13 01:30:20 +0100

    setting up database for testing instance using mockgoose and installing mockgoose package

---
+++ user_test/user_auth_spec.js
@@ -0,0 +1,27 @@
+/*jshint esversion: 6 */
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+const expect = require('chai').expect;
+const app = require('../app');
+
+
+// section to test for registering user
+describe('/POST user', () => {
+    it('it should not POST a user without  field', (done) => {
+        let user = {
+            name: "Ajayi Oluwafemi",
+            email: "test@gamil.com",
+            password: 1954,
+            createdAt:Date.now()
+        }
+      chai.request(server)
+          .post('/signup')
+          .send(user)
+          .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('errors');
+            done();
+          });
+    });
+});
