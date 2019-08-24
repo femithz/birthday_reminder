b7c3abd85472eb4355d61c06a5d115a85d5c58ec
Ajayi Oluwafemi Adeyinka <ajayiadeyinka6991@gmail.com>
2019-08-24 23:08:40 +0100

    endpoint to create contact is been created

--- routes/contact.js
+++ routes/contact.js
@@ -12,8 +12,21 @@ router.post('/contacts', function(req,res,next) {
       name:req.body.name,
       email:req.body.email,
       address:req.body.address,
-      birthday:req.body.address,
+      birthday:req.body.birthday,
+      phoneNumber:req.body.phoneNumber,
+      createdAt:Date.now(),
     });
-    console.log(contact);
+    contact.save(function (err,result) {
+      if(err) { 
+        return res.status(501).json({
+          message:'New Contact cannot be creaeted'
+        })
+       } else {
+         return res.status(200).json({
+           contact:result,
+           message:'You have successfully add a new contact'
+         })
+       }
+    })
 })
 module.exports = router;
