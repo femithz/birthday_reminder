29472ff3de835daf6c6a11e3b292ab1c4716fea5
Ajayi Oluwafemi Adeyinka <ajayiadeyinka6991@gmail.com>
2019-09-13 10:40:39 +0100

    test for contact creation is added

---
+++ user_test/contact_spec.js
@@ -0,0 +1,23 @@
+/*jshint esversion: 6 */
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+const expect = require('chai').expect;
+const app = require('../app');
+
+
+describe('POST contact', () => {
+    it('it should not POST a user without  field', (done) => {
+        let contact = {
+            name: "Ajayi Oluwafemi",
+            email: "test@gamil.com",
+            address: "No 28,Michigan,America",
+            birthday:  "29-02-2016",
+            phoneNumber:  08145019560,
+            contactOwner: req.user._id,
+            createdAt:Date.now(),
+        }
+        chai.request(app)
+        .post('/')
+        .send(contact)
+    })
+})
